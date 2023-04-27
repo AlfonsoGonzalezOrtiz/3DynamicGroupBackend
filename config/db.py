167dbb10a685feb6201bafe623af80a5551a417b
Alfonso Gonzalez <alfonsogonzalezortiz@uma.es>
2023-04-27 20:51:44 +0200

    db

--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://0619973969:WkpJdIEvnBXcWhUn@cluster0.qeymtn1.mongodb.net/test")
+client = MongoClient("mongodb+srv://0619973969:WkpJdIEvnBXcWhUn@cluster0.qeymtn1.mongodb.net/?retryWrites=true&w=majority")
 db = client.dbDynamicGroup
 
