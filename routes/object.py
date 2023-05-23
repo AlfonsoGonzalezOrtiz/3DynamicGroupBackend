5f4197065f7cd405c78d81b87f1541f483100510
Alfonso Gonzalez <alfonsogonzalezortiz@uma.es>
2023-05-23 09:28:06 +0200

    Complete api

--- routes/object.py
+++ routes/object.py
@@ -22,3 +22,51 @@ def get_object(code:str, request: Request):
     except:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"object with code {code} not found")
 
+'''CREATE object'''
+@routerobject.post("/", response_description="Create a new object", status_code=status.HTTP_201_CREATED, response_model=object)
+def create_object(request: Request,object = Body(...)):
+    object = jsonable_encoder(object)
+
+    new_object = db[collection].insert_one(object)
+    created_object = db[collection].find_one(
+        {"_id": new_object.inserted_id}
+    )
+
+    return created_object
+
+'''DELETE object'''
+@routerobject.delete("/{codigo}", response_description="Delete a object")
+def delete_object(codigo:str, request: Request, response: Response):
+
+    try:
+        object_deleted = db[collection].delete_one({"codigo": codigo})
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"object with ID {codigo} not found")
+
+    if object_deleted.deleted_count == 1:
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return response
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"object with ID {codigo} not found")
+
+
+'''UPDATE object'''
+@routerobject.put("/{codigo}", response_description="Update a object", response_model=object)
+def update_object(codigo:str, request: Request, data: object = Body(...)):
+
+    object = {k: v for k, v in data.dict().items() if v is not None}
+    
+    if len(object) >= 1:
+        update_result = db[collection].update_one(
+            {"codigo": codigo}, {"$set": object}
+        )
+
+        if update_result.modified_count == 0:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"object with ID {codigo} not modified")
+
+    if (
+        existing_object := db[collection].find_one({"codigo":codigo})
+    ) is not None:
+        return existing_object
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"object with ID {codigo} not found")
